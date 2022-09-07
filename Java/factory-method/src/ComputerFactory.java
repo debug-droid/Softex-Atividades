8d70d8b9a1c15d4babdd33899165350951345bbb
Diego Augusto <DiegoprogBr@hotmail.com>
2022-09-07 11:18:23 -0300

    métodos finalizados

--- Java/factory-method/src/ComputerFactory.java
+++ Java/factory-method/src/ComputerFactory.java
@@ -1,13 +1,11 @@
 public class ComputerFactory implements iComputer {
+
+	public static Server createComputer() {
+		return new Server("8192", "1 Tb", "3.90Ghz", "server");
+	}
 	
-	public static void createComputer(String ram, String hdd, String cpu, String type) {
-		if(type == "pc") {
-			Pc pc = new Pc("4096", "500Gb", "3.50Ghz", "pc");
-			System.out.println(pc);
-		} else {
-			Server server = new Server("8192", "1 Tb", "3.90Ghz", "server");
-			System.out.println(server);
-		}
+	public static Pc createrComputer() {
+		return new Pc("4096", "500Gb", "3.50Ghz", "pc");
 	}
 	
 }
--- Java/factory-method/src/ComputerFactoryApp.java
+++ Java/factory-method/src/ComputerFactoryApp.java
@@ -3,11 +3,11 @@ public class ComputerFactoryApp {
 
 	public static void main(String[] args) {
 		
-		Pc pc = ComputerFactory.createComputer();
-		System.out.println(pc);
-		
 		Server server = ComputerFactory.createComputer();
 		System.out.println(server);
+		
+		Pc pc = ComputerFactory.createrComputer();
+		System.out.println(pc);
 
 	}
 
