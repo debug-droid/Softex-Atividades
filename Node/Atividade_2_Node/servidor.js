0e8b4507ef2332f8684e8afbc5b9939a1c0b8e63
Diego Augusto <DiegoprogBr@hotmail.com>
2022-11-13 11:35:03 -0300

    atividade completa

--- Node/Atividade_2_Node/servidor.js
+++ Node/Atividade_2_Node/servidor.js
@@ -1,18 +1,14 @@
 //usando a biblioteca http do node
-const { criarServer } = require('http');
+const http = require('http');
 
 //constante para configuração da porta
 const PORTA = process.env.PORT || 3000;
 
-//variável principal do servidor
-const servidor = criarServer();
-
-//quando houver uma requisição ao servidor, será respondido com 'Olá Mundo!'
-servidor.on('request', (request, response) => {
-    response.end('Olá Mundo!');
-})
+//cria o servidor http e escuta uma requisição para a porta 3000
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type':'text/plain'});
+    res.end('Olá Mundo!');
+}).listen(3000, '127.0.0.1');
 
 //mensagem do servidor quando estiver rodando
-servidor.listen(PORT, () => {
-    console.log(`Servidor na porta ${PORTA}`);
-})
+console.log('Servidor rodando em http://127.0.0.1/' + `${PORTA}`);
